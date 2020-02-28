ff33460dbed40dc732b15f8bcfebe728fef579b9
Junta <j.sekimori@gmail.com>
2020-02-28 22:06:38 +1100

    Assignment complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions here: makeCacheMatrix() and cacheSolve().
+## Together, these functions return the inverse of a matrix and stores it to cache.
+## Any repeat calls of the function will retrieve the value from cache, saving us from re-computing it.
 
-## Write a short comment describing this function
+## In the first function below, we pass in a square matrix.
+## The function stores the matrix values and defines functions that allows us to store and retreive again the inverse when the second function is called.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverted) inv <<- inverted
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function is the child function that does the work of inverting the matrix set using the first function.
+## It calls functions from its parent environment to check for whether the inverse has already been calculated and set.
+## If it has, the function returns it from cache and logs a message to that effect.
+## If it hasn't, it calculates the inverse and sets it to the 'inv' symbol in the parent environment, available to access in future.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix <- x$get()
+  inv <- solve(matrix, ...)
+  x$setinverse(inv)
+  inv  
 }
+
+## Try this for example by running the following:
+## testmatrix <- matrix(1:4,2,2)
+## cachedMatrix <- makeCacheMatrix(testmatrix)
+## cacheSolve(cachedMatrix)
